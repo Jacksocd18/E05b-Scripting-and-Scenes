96a109fff4e7a3f7351a7b3d48dd971d84e2b4ff
Jacksocd18 <jacksocd@iu.edu>
2020-02-18 00:36:18 -0500

    Scripts-and-Scenes

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,15 +1,26 @@
 extends RigidBody2D
 
+export var maxspeed = 300
+
+signal lives
+signal score
+
 func _ready():
 	contact_monitor = true
 	set_max_contacts_reported(4)
+	var Worldnode = get_node("/root/World")
+	connect("score", Worldnode, "increase_score")
+	connect("lives", Worldnode, "decrease_lives")
 
 func _physics_process(delta):
 	var bodies = get_colliding_bodies()
 	for body in bodies:
 		if body.is_in_group("Tiles"):
+			emit_signal("score",body.score)
 			body.queue_free()
+		if body.get_name() == "Paddle":
+			pass
 	
 	if position.y > get_viewport_rect().end.y:
+		emit_signal("lives")
 		queue_free()
-		print("Died")
